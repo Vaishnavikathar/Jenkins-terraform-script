35842e3e50c184663eeafbb0f47c29b3b3ee00be
Vaishnavi Kathar <vaishnavikathar786@gmail.com>
2025-12-05 20:49:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,39 +1,9 @@
-variable "PROJECT_ID" {
+variable "project_id" {
   description = "Google Cloud project ID"
   type        = string
 }
 
-variable "region" {
-  description = "Region"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "zone" {
-  description = "Zone"
-  type        = string
-  default     = "us-central1-a"
-}
-
-variable "GOOGLE_CREDENTIALS" {
+variable "credentials_file" {
   description = "Path to SA JSON"
   type        = string
-}
-
-variable "instance_name" {
-  description = "VM name"
-  type        = string
-  default     = "jenkins-tf-instance"
-}
-
-variable "machine_type" {
-  description = "Machine type"
-  type        = string
-  default     = "e2-medium"
-}
-
-variable "image" {
-  description = "OS image"
-  type        = string
-  default     = "debian-cloud/debian-11"
 }
