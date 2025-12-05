dc027e9c1237b5fd9d5a138b9559139117f692ff
Vaishnavi Kathar <vaishnavikathar786@gmail.com>
2025-12-05 20:54:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,37 @@ variable "project_id" {
   type        = string
 }
 
-variable "credentials_file" {
-  description = "Path to SA JSON"
+variable "region" {
+  description = "Region"
   type        = string
+  default     = "asia-south1"
+}
+
+variable "zone" {
+  description = "Zone"
+  type        = string
+  default     = "asia-south1-a"
+}
+
+variable "credentials_file" {
+  description = "Path to service account JSON"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "VM instance name"
+  type        = string
+  default     = "jenkins-tf-instance"
+}
+
+variable "machine_type" {
+  description = "Machine type"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "image" {
+  description = "OS image"
+  type        = string
+  default     = "debian-cloud/debian-11"
 }
